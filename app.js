cb0d7d1839b57f265210e7e70da3ae1fa979fc25
ibegei@forceoft.com <ibegei@forceoft.com>
2019-08-22 19:23:10 +0300

    make it better

--- app.js
+++ app.js
@@ -572,6 +572,7 @@ app.post('/dataForUpdateGitLab', function(req, res) {
 
 
 						console.log('repos',repos);
+						console.log('repos.length',repos.length);
 						if(repos.length != 0){
 							console.log('REEEEEEEEEEEEPPPPPPPPPPPPOOOOOOOOOOOOO');
 							Array.from(repos).forEach(function(repo, repIndex, repArray) {
